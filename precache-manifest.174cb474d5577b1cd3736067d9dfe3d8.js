848bca1e37b3c1593387c62028af684bc63e2e00
arodriguezo <arodriguezo@na-at.com.mx>
2021-08-26 19:21:16 -0500

    Updates

---
+++ precache-manifest.174cb474d5577b1cd3736067d9dfe3d8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e840dd237f57d596c6d",
+    "url": "/AlejandroRodriguez/static/css/main.4d56f1eb.chunk.css"
+  },
+  {
+    "revision": "3e840dd237f57d596c6d",
+    "url": "/AlejandroRodriguez/static/js/main.3e840dd2.chunk.js"
+  },
+  {
+    "revision": "90c7e430d5119ff67194",
+    "url": "/AlejandroRodriguez/static/js/1.90c7e430.chunk.js"
+  },
+  {
+    "revision": "7e66c91de90d338c27d6",
+    "url": "/AlejandroRodriguez/static/js/runtime~main.7e66c91d.js"
+  },
+  {
+    "revision": "060bcd8156d244528f692e59599bc1b8",
+    "url": "/AlejandroRodriguez/static/media/profilepic.060bcd81.jpeg"
+  },
+  {
+    "revision": "0673e4fe1449eb28da6e9738479fd921",
+    "url": "/AlejandroRodriguez/static/media/cv.0673e4fe.pdf"
+  },
+  {
+    "revision": "599eb2030b5617f060e91dc069ad5b3b",
+    "url": "/AlejandroRodriguez/static/media/sociomas.599eb203.jpeg"
+  },
+  {
+    "revision": "b1704d72b6dca9f2855b5ed1e489e393",
+    "url": "/AlejandroRodriguez/static/media/azzheztia.b1704d72.jpeg"
+  },
+  {
+    "revision": "2d491ba6a9695f84e4f463dc7f4c9bbd",
+    "url": "/AlejandroRodriguez/static/media/SMNYL.2d491ba6.png"
+  },
+  {
+    "revision": "1d45b6cee96ec31748de8c1c63987108",
+    "url": "/AlejandroRodriguez/static/media/swift.1d45b6ce.png"
+  },
+  {
+    "revision": "302690e102dbc0712d0b99914f527956",
+    "url": "/AlejandroRodriguez/static/media/swiftui.302690e1.png"
+  },
+  {
+    "revision": "58c2bb5f534b6a171df2529092770e92",
+    "url": "/AlejandroRodriguez/static/media/flutter.58c2bb5f.png"
+  },
+  {
+    "revision": "a7212ed4c02ca28a98c310a947c21998",
+    "url": "/AlejandroRodriguez/static/media/react.a7212ed4.png"
+  },
+  {
+    "revision": "2b62678dd69bfdfcf7272fdc54b42af2",
+    "url": "/AlejandroRodriguez/index.html"
+  }
+];
